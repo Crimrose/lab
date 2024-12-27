8edf202449d71fd2f67f0aa6731f347fa6b0ff60
Kien Tran <manmelody193@gmail.com>
2024-12-27 15:33:38 +0700

    chore: Fix host

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'db';
-$db   = '127.0.0.1';
-$user = 'myuser';
-$pass = 'mypassword';
+$host = '127.0.0.1';
+$db   = 'mydatabase';
+$user = 'root';
+$pass = 'mysql_root_password';
 $dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
 try {
