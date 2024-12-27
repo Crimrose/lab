326a1dfdfd756bfb25a67505b2bf7213f48f7eb0
Kien Tran <manmelody193@gmail.com>
2024-12-27 15:30:12 +0700

    chore: Change structure

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'db';
+$db   = 'mydatabase';
+$user = 'myuser';
+$pass = 'mypassword';
+$dsn  = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    echo "Connected to MySQL successfully!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
